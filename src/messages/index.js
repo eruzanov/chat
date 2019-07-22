f0b0d173a1e5ba99b6d90add02d1344be7b571ce
EugeneRuzanov <eugene.ruzanov@gmail.com>
2019-07-22 19:33:35 +0300

    http query

--- src/messages/index.js
+++ src/messages/index.js
@@ -6,21 +6,29 @@ const arrText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ali
   ' ',
 );
 
+const COUNT = 20;
+
+function prepare(it, i) {
+  return {
+    id: i,
+    iconUrl: it.picture.thumbnail,
+    author: `${it.name.first} ${it.name.last}`,
+    time: new Date(),
+    text: arrText
+      .sort(() => Math.random() - 0.5)
+      .slice(0, Math.random() * arrText.length + 1)
+      .join(' '),
+  }
+}
+
 export const moduleName = angular
   .module('messages', [])
   .component('messagesList', {
     template: list,
-    controller: $scope => {
-      $scope.messages = new Array(20).fill(true).map((_, i) => ({
-        id: i,
-        iconUrl: 'https://randomuser.me/api/portraits/women/60.jpg',
-        author: 'Genesis Arnold',
-        time: new Date(),
-        text: arrText
-          .sort(() => Math.random() - 0.5)
-          .slice(0, Math.random() * arrText.length + 1)
-          .join(' '),
-      }));
+    controller: ['$scope', '$http', ($scope, $http) => {
+      $http
+        .get(`https://randomuser.me/api/?results=${COUNT}`)
+        .then(resp => $scope.messages = resp.data.results.map(prepare));
       //todo sort by date
-    },
+    }],
   }).name;
