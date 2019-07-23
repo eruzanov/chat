71be3d0b0919a76999c82d2f25b1ca1f7d5aeebb
EugeneRuzanov <eugene.ruzanov@gmail.com>
2019-07-24 02:44:36 +0300

    added a favicon

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      favicon: './src/favicon.png'
     }),
   ],
   devServer: {
