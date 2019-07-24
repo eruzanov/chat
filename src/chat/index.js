9ab0b09a53fbf1b398899fced0a81e3b9bfb944d
EugeneRuzanov <eugene.ruzanov@gmail.com>
2019-07-24 22:22:48 +0300

    selected chat by hash

--- src/chat/index.js
+++ src/chat/index.js
@@ -2,23 +2,26 @@ import angular from 'angular';
 import chatList from './chat-list.html';
 import './chat-list.css';
 
+const chats = [
+  'angular',
+  'jobs',
+  'nodejs',
+  'offtopic',
+  'react',
+  'typescript',
+  'vue',
+];
+
 export const moduleName = angular.module('chat', []).component('chatList', {
   template: chatList,
   controller: [
     '$scope',
     '$rootScope',
-    function($scope, $rootScope) {
-      $scope.chatList = [
-        'angular',
-        'jobs',
-        'nodejs',
-        'offtopic',
-        'react',
-        'typescript',
-        'vue',
-      ];
-
-      $rootScope.selected = $scope.chatList[0];
+    '$location',
+    function($scope, $rootScope, $location) {
+      const selected = $location.hash();
+      $scope.chatList = chats;
+      $rootScope.selected = chats.includes(selected) ? selected : $scope.chatList[0];
 
       this.onSelect = chat => ($rootScope.selected = chat);
     },
